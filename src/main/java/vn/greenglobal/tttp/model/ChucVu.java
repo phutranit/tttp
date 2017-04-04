c5a97cd9dec587ce3a2c7ab07a7833e89f7516c6
NamTVD <namtvd@greenglobal.vn>
2017-04-04 14:24:57 +0000

    feat(TTTP): Tao model ChucVu
    Tao model ChucVu
    
    see #1

---
+++ src/main/java/vn/greenglobal/tttp/model/ChucVu.java
@@ -0,0 +1,45 @@
+package vn.greenglobal.tttp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import io.katharsis.resource.annotations.JsonApiResource;
+
+@Entity
+@Table(name = "chucvu")
+@JsonApiResource(type = "chucvus")
+@Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
+public class ChucVu extends Model<ChucVu> {
+
+	private String ma = "";
+	private String ten = "";
+	private String moTa = "";
+
+	public String getMa() {
+		return ma;
+	}
+
+	public void setMa(String ma) {
+		this.ma = ma;
+	}
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+}
