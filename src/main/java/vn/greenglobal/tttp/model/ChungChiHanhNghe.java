3b903feec3091addaa16a7897a82412b0e30b07f
NamTVD <namtvd@greenglobal.vn>
2017-04-04 15:16:17 +0000

    feat(TTTP): Them truong congDan trong model ChungChiHanhNghe
    Them truong cong dan trong  model ChungChiHanhNghe
    
    see #1

--- src/main/java/vn/greenglobal/tttp/model/ChungChiHanhNghe.java
+++ src/main/java/vn/greenglobal/tttp/model/ChungChiHanhNghe.java
@@ -1,12 +1,15 @@
 package vn.greenglobal.tttp.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import io.katharsis.resource.annotations.JsonApiIncludeByDefault;
 import io.katharsis.resource.annotations.JsonApiResource;
+import io.katharsis.resource.annotations.JsonApiToOne;
 
 @Entity
 @Table(name = "chungchihanhnghe")
@@ -17,6 +20,11 @@ public class ChungChiHanhNghe extends Model<ChungChiHanhNghe> {
 	private String ma = "";
 	private String ten = "";
 	private String moTa = "";
+
+	@ManyToOne
+	@JsonApiToOne
+	@JsonApiIncludeByDefault
+	private CongDan congDan;
 
 	public String getMa() {
 		return ma;
@@ -40,6 +48,14 @@ public class ChungChiHanhNghe extends Model<ChungChiHanhNghe> {
 
 	public void setMoTa(String moTa) {
 		this.moTa = moTa;
+	}
+
+	public CongDan getCongDan() {
+		return congDan;
+	}
+
+	public void setCongDan(CongDan congDan) {
+		this.congDan = congDan;
 	}
 
 }
