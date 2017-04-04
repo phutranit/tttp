6f75896b7f007019cc859df17204569dc5d7ceba
NamTVD <namtvd@greenglobal.vn>
2017-04-04 14:25:41 +0000

    feat(TTTP): Tao model DanToc
    Tao model DanToc
    
    see #1

---
+++ src/main/java/vn/greenglobal/tttp/model/DanToc.java
@@ -0,0 +1,64 @@
+package vn.greenglobal.tttp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import io.katharsis.resource.annotations.JsonApiResource;
+
+@Entity
+@Table(name = "dantoc")
+@JsonApiResource(type = "dantocs")
+@Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
+public class DanToc extends Model<DanToc> {
+
+	private String ma = "";
+	private String ten = "";
+	private String tenKhac = "";
+	private String moTa = "";
+
+	private boolean thieuSo;
+
+	public String getMa() {
+		return ma;
+	}
+
+	public void setMa(String ma) {
+		this.ma = ma;
+	}
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getTenKhac() {
+		return tenKhac;
+	}
+
+	public void setTenKhac(String tenKhac) {
+		this.tenKhac = tenKhac;
+	}
+
+	public boolean isThieuSo() {
+		return thieuSo;
+	}
+
+	public void setThieuSo(boolean thieuSo) {
+		this.thieuSo = thieuSo;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+}
