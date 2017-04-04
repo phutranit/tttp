f828769f5495702f2efb216b61d089fe653a1a63
NamTVD <namtvd@greenglobal.vn>
2017-04-04 14:28:23 +0000

    feat(TTTP): Them truong ID vao class model
    Them truong ID vao class model
    
    see #1

--- src/main/java/vn/greenglobal/tttp/model/LoaiVanBan.java
+++ src/main/java/vn/greenglobal/tttp/model/LoaiVanBan.java
@@ -1,15 +1,11 @@
 package vn.greenglobal.tttp.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import io.katharsis.resource.annotations.JsonApiId;
 import io.katharsis.resource.annotations.JsonApiResource;
 
 @Entity
@@ -17,23 +13,10 @@ import io.katharsis.resource.annotations.JsonApiResource;
 @JsonApiResource(type = "loaivanbans")
 @Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
 public class LoaiVanBan extends Model<LoaiVanBan> {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@JsonApiId
-	private Long id;
+
 	private int soThuTu;
 	private String ten = "";
 	private String moTa = "";
-	
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 	
 	public int getSoThuTu() {
 		return soThuTu;
--- src/main/java/vn/greenglobal/tttp/model/Model.java
+++ src/main/java/vn/greenglobal/tttp/model/Model.java
@@ -2,21 +2,39 @@ package vn.greenglobal.tttp.model;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 //import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.common.base.Strings;
 
+import io.katharsis.resource.annotations.JsonApiId;
+
 @MappedSuperclass
 public class Model<T extends Model<T>> {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonApiId
+	private Long id;
+	
 	private String trangThai;
+	
 	private LocalDateTime ngayTao;
 	private LocalDateTime ngaySua;
+	
 	private boolean daXoa;
 	
-	@Column
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getTrangThai() {
 		return trangThai;
 	}
@@ -24,9 +42,8 @@ public class Model<T extends Model<T>> {
 	public void setTrangThai(final String _trangThai) {
 		trangThai = Strings.nullToEmpty(_trangThai);
 	}
-	
-	@Column
-	//@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS+0000")
+
+//	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS+0000")
 	public LocalDateTime getNgaySua() {
 		return this.ngaySua;
 	}
@@ -34,9 +51,8 @@ public class Model<T extends Model<T>> {
 	public void setNgaySua(LocalDateTime ngaySua1) {
 		this.ngaySua = ngaySua1;
 	}
-	
-	@Column
-	//@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS+0000")
+
+//	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS+0000")
 	public LocalDateTime getNgayTao() {
 		return this.ngayTao;
 	}
@@ -46,7 +62,6 @@ public class Model<T extends Model<T>> {
 		this.ngayTao = ngayTao1;
 	}
 
-	@Column
 	public boolean isDaXoa() {
 		return daXoa;
 	}
