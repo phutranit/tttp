8d3886390077211218d632cf2fcb2b1a26e5ad92
NamTVD <namtvd@greenglobal.vn>
2017-04-04 14:26:41 +0000

    feat(TTTP): Tao model ToDanPho
    Tao model ToDanPho
    
    see #1

---
+++ src/main/java/vn/greenglobal/tttp/model/ToDanPho.java
@@ -0,0 +1,52 @@
+package vn.greenglobal.tttp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import io.katharsis.resource.annotations.JsonApiIncludeByDefault;
+import io.katharsis.resource.annotations.JsonApiResource;
+import io.katharsis.resource.annotations.JsonApiToOne;
+
+@Entity
+@Table(name = "todanpho")
+@JsonApiResource(type = "todanphos")
+@Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
+public class ToDanPho extends Model<ToDanPho> {
+
+	private String ten = "";
+	private String moTa = "";
+
+	@ManyToOne
+	@JsonApiToOne
+	@JsonApiIncludeByDefault
+	private DonViHanhChinh donViHanhChinh;
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+	public DonViHanhChinh getDonViHanhChinh() {
+		return donViHanhChinh;
+	}
+
+	public void setDonViHanhChinh(DonViHanhChinh donViHanhChinh) {
+		this.donViHanhChinh = donViHanhChinh;
+	}
+
+}
