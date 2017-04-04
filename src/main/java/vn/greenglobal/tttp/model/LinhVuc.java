078dc8c64ffe4c4df1bc6c5694660b60ec4dd2e5
NamTVD <namtvd@greenglobal.vn>
2017-04-04 15:05:13 +0000

    feat(TTTP): Tao model LinhVuc
    Tao model LinhVuc
    
    see #1

---
+++ src/main/java/vn/greenglobal/tttp/model/LinhVuc.java
@@ -0,0 +1,71 @@
+package vn.greenglobal.tttp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import io.katharsis.resource.annotations.JsonApiIncludeByDefault;
+import io.katharsis.resource.annotations.JsonApiResource;
+import io.katharsis.resource.annotations.JsonApiToOne;
+
+@Entity
+@Table(name = "linhvuc")
+@JsonApiResource(type = "linhvucs")
+@Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
+public class LinhVuc extends Model<LinhVuc> {
+
+	private String ma = "";
+	private String ten = "";
+	private String moTa = "";
+
+	private boolean linhVucKhac;
+
+	@ManyToOne
+	@JsonApiToOne
+	@JsonApiIncludeByDefault
+	private LinhVuc cha;
+
+	public String getMa() {
+		return ma;
+	}
+
+	public void setMa(String ma) {
+		this.ma = ma;
+	}
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+	public boolean isLinhVucKhac() {
+		return linhVucKhac;
+	}
+
+	public void setLinhVucKhac(boolean linhVucKhac) {
+		this.linhVucKhac = linhVucKhac;
+	}
+
+	public LinhVuc getCha() {
+		return cha;
+	}
+
+	public void setCha(LinhVuc cha) {
+		this.cha = cha;
+	}
+
+}
