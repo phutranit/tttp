18c23b41c9cd637a97a2c1251b2928cb4d034ca9
NamTVD <namtvd@greenglobal.vn>
2017-04-07 10:20:15 +0000

    feat(TTTP): Cap nhat model Don
    Cap nhat model Don
    
    see #1

--- src/main/java/vn/greenglobal/tttp/model/Don.java
+++ src/main/java/vn/greenglobal/tttp/model/Don.java
@@ -21,15 +21,87 @@ public class Don extends Model<Don> {
 	private String ma = "";
 	private String noiDUng = "";
 	private String yeuCauCuaCongDan = "";
-	
+
 	private int soLanKhieuNaiToCao = 0;
 	private int soNguoi;
-	
+
 	private LocalDateTime ngayTiepNhan;
-	
+
 	private Don donLanTruoc;
-	
+
 	private LoaiDonEnum loaiDon;
 	private NguonTiepNhanDonEnum nguonTiepNhanDon;
-	
+
+	public String getMa() {
+		return ma;
+	}
+
+	public void setMa(String ma) {
+		this.ma = ma;
+	}
+
+	public String getNoiDUng() {
+		return noiDUng;
+	}
+
+	public void setNoiDUng(String noiDUng) {
+		this.noiDUng = noiDUng;
+	}
+
+	public String getYeuCauCuaCongDan() {
+		return yeuCauCuaCongDan;
+	}
+
+	public void setYeuCauCuaCongDan(String yeuCauCuaCongDan) {
+		this.yeuCauCuaCongDan = yeuCauCuaCongDan;
+	}
+
+	public int getSoLanKhieuNaiToCao() {
+		return soLanKhieuNaiToCao;
+	}
+
+	public void setSoLanKhieuNaiToCao(int soLanKhieuNaiToCao) {
+		this.soLanKhieuNaiToCao = soLanKhieuNaiToCao;
+	}
+
+	public int getSoNguoi() {
+		return soNguoi;
+	}
+
+	public void setSoNguoi(int soNguoi) {
+		this.soNguoi = soNguoi;
+	}
+
+	public LocalDateTime getNgayTiepNhan() {
+		return ngayTiepNhan;
+	}
+
+	public void setNgayTiepNhan(LocalDateTime ngayTiepNhan) {
+		this.ngayTiepNhan = ngayTiepNhan;
+	}
+
+	public Don getDonLanTruoc() {
+		return donLanTruoc;
+	}
+
+	public void setDonLanTruoc(Don donLanTruoc) {
+		this.donLanTruoc = donLanTruoc;
+	}
+
+	public LoaiDonEnum getLoaiDon() {
+		return loaiDon;
+	}
+
+	public void setLoaiDon(LoaiDonEnum loaiDon) {
+		this.loaiDon = loaiDon;
+	}
+
+	public NguonTiepNhanDonEnum getNguonTiepNhanDon() {
+		return nguonTiepNhanDon;
+	}
+
+	public void setNguonTiepNhanDon(NguonTiepNhanDonEnum nguonTiepNhanDon) {
+		this.nguonTiepNhanDon = nguonTiepNhanDon;
+	}
+
 }
