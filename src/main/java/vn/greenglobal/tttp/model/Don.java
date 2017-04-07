3f1b6aa95ab100e81d12c3d15300f624ab6cb108
NamTVD <namtvd@greenglobal.vn>
2017-04-07 10:13:52 +0000

    feat(TTTP): Cap nhat model Don
    Cap nhat model Don
    
    see #1

--- src/main/java/vn/greenglobal/tttp/model/Don.java
+++ src/main/java/vn/greenglobal/tttp/model/Don.java
@@ -1,5 +1,7 @@
 package vn.greenglobal.tttp.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -7,6 +9,8 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import io.katharsis.resource.annotations.JsonApiResource;
+import vn.greenglobal.tttp.enums.LoaiDonEnum;
+import vn.greenglobal.tttp.enums.NguonTiepNhanDonEnum;
 
 @Entity
 @Table(name = "don")
@@ -14,5 +18,18 @@ import io.katharsis.resource.annotations.JsonApiResource;
 @Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Don extends Model<Don> {
 
+	private String ma = "";
+	private String noiDUng = "";
+	private String yeuCauCuaCongDan = "";
+	
+	private int soLanKhieuNaiToCao = 0;
+	private int soNguoi;
+	
+	private LocalDateTime ngayTiepNhan;
+	
+	private Don donLanTruoc;
+	
+	private LoaiDonEnum loaiDon;
+	private NguonTiepNhanDonEnum nguonTiepNhanDon;
 	
 }
