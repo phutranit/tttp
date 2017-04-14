d615fe482a302d13897613fbec985eea5cda1eb6
NamTVD <namtvd@greenglobal.vn>
2017-04-14 10:26:45 +0000

    feat(CapCoQuanQuanLy): tao api danh muc cap co quan quan ly
    Tao api danh muc cap co quan quan ly
    
    see #11

---
+++ src/main/java/vn/greenglobal/tttp/controller/CapCoQuanQuanLyController.java
@@ -0,0 +1,134 @@
+package vn.greenglobal.tttp.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import vn.greenglobal.core.model.common.BaseController;
+import vn.greenglobal.core.model.common.BaseRepository;
+import vn.greenglobal.tttp.enums.ApiErrorEnum;
+import vn.greenglobal.tttp.model.CapCoQuanQuanLy;
+import vn.greenglobal.tttp.repository.CapCoQuanQuanLyRepository;
+import vn.greenglobal.tttp.service.CapCoQuanQuanLyService;
+import vn.greenglobal.tttp.util.Utils;
+
+@RestController
+@RepositoryRestController
+@Api(value = "capCoQuanQuanLys", description = "Cấp Cơ Quan Quản Lý")
+public class CapCoQuanQuanLyController extends BaseController<CapCoQuanQuanLy> {
+
+	private static Log log = LogFactory.getLog(CapCoQuanQuanLyController.class);
+	private static CapCoQuanQuanLyService capCoQuanQuanLyService = new CapCoQuanQuanLyService();
+
+	@Autowired
+	private CapCoQuanQuanLyRepository repo;
+
+	public CapCoQuanQuanLyController(BaseRepository<CapCoQuanQuanLy, Long> repo) {
+		super(repo);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(method = RequestMethod.GET, value = "/capCoQuanQuanLys")
+	@ApiOperation(value = "Lấy danh sách Cấp Cơ Quan Quản Lý", position = 1, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody PagedResources<CapCoQuanQuanLy> getList(Pageable pageable,
+			@RequestParam(value = "tuKhoa", required = false) String tuKhoa,
+			@RequestParam(value = "cha", required = false) Long cha, PersistentEntityResourceAssembler eass) {
+		log.info("Get danh sach CapCoQuanQuanLy");
+
+		Page<CapCoQuanQuanLy> page = repo.findAll(capCoQuanQuanLyService.predicateFindAll(tuKhoa, cha), pageable);
+		return assembler.toResource(page, (ResourceAssembler) eass);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/capCoQuanQuanLys")
+	@ApiOperation(value = "Thêm mới Cấp Cơ Quan Quản Lý", position = 2, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Thêm mới Cấp Cơ Quan Quản Lý thành công", response = CapCoQuanQuanLy.class),
+			@ApiResponse(code = 201, message = "Thêm mới Cấp Cơ Quan Quản Lý thành công", response = CapCoQuanQuanLy.class) })
+	public ResponseEntity<Object> create(@RequestBody CapCoQuanQuanLy capCoQuanQuanLy,
+			PersistentEntityResourceAssembler eass) {
+		log.info("Tao moi CapCoQuanQuanLy");
+
+		if (StringUtils.isNotBlank(capCoQuanQuanLy.getMa()) && StringUtils.isNotBlank(capCoQuanQuanLy.getTen())
+				&& capCoQuanQuanLyService.checkExistsData(repo, capCoQuanQuanLy)) {
+			return Utils.responseErrors(HttpStatus.BAD_REQUEST, ApiErrorEnum.MA_TEN_EXISTS.name(),
+					ApiErrorEnum.MA_TEN_EXISTS.getText());
+		}
+		return Utils.doSave(repo, capCoQuanQuanLy, eass, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/capCoQuanQuanLys/{id}")
+	@ApiOperation(value = "Lấy Cấp Cơ Quan Quản Lý theo Id", position = 3, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Lấy Cấp Cơ Quan Quản Lý thành công", response = CapCoQuanQuanLy.class) })
+	public ResponseEntity<PersistentEntityResource> getCapCoQuanQuanLy(@PathVariable("id") long id,
+			PersistentEntityResourceAssembler eass) {
+		log.info("Get CapCoQuanQuanLy theo id: " + id);
+
+		CapCoQuanQuanLy capCoQuanQuanLy = repo.findOne(capCoQuanQuanLyService.predicateFindOne(id));
+		if (capCoQuanQuanLy == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(eass.toFullResource(capCoQuanQuanLy), HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.PATCH, value = "/capCoQuanQuanLys/{id}")
+	@ApiOperation(value = "Cập nhật Cấp Cơ Quan Quản Lý", position = 4, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Cập nhật Cấp Cơ Quan Quản Lý thành công", response = CapCoQuanQuanLy.class) })
+	public @ResponseBody ResponseEntity<Object> update(@PathVariable("id") long id,
+			@RequestBody CapCoQuanQuanLy capCoQuanQuanLy, PersistentEntityResourceAssembler eass) {
+		log.info("Update ThamQuyenGiaiQuyet theo id: " + id);
+		
+		capCoQuanQuanLy.setId(id);
+		
+		if (StringUtils.isNotBlank(capCoQuanQuanLy.getMa()) && StringUtils.isNotBlank(capCoQuanQuanLy.getTen())
+				&& capCoQuanQuanLyService.checkExistsData(repo, capCoQuanQuanLy)) {
+			return Utils.responseErrors(HttpStatus.BAD_REQUEST, ApiErrorEnum.MA_TEN_EXISTS.name(),
+					ApiErrorEnum.MA_TEN_EXISTS.getText());
+		}
+
+		if (!capCoQuanQuanLyService.isExists(repo, id)) {
+			return Utils.responseErrors(HttpStatus.NOT_FOUND, ApiErrorEnum.DATA_NOT_FOUND.name(),
+					ApiErrorEnum.DATA_NOT_FOUND.getText());
+		}
+
+		return Utils.doSave(repo, capCoQuanQuanLy, eass, HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/capCoQuanQuanLys/{id}")
+	@ApiOperation(value = "Xoá Cấp Cơ Quan Quản Lý", position = 5, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = { @ApiResponse(code = 204, message = "Xoá Cấp Cơ Quan Quản Lý thành công") })
+	public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
+		log.info("Delete CapCoQuanQuanLy theo id: " + id);
+
+		CapCoQuanQuanLy capCoQuanQuanLy = capCoQuanQuanLyService.deleteCapCoQuanQuanLy(repo, id);
+		if (capCoQuanQuanLy == null) {
+			return Utils.responseErrors(HttpStatus.NOT_FOUND, ApiErrorEnum.DATA_NOT_FOUND.name(),
+					ApiErrorEnum.DATA_NOT_FOUND.getText());
+		}
+		repo.save(capCoQuanQuanLy);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
---
+++ src/main/java/vn/greenglobal/tttp/service/CapCoQuanQuanLyService.java
@@ -0,0 +1,64 @@
+package vn.greenglobal.tttp.service;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import vn.greenglobal.tttp.model.CapCoQuanQuanLy;
+import vn.greenglobal.tttp.model.QCapCoQuanQuanLy;
+import vn.greenglobal.tttp.repository.CapCoQuanQuanLyRepository;
+
+public class CapCoQuanQuanLyService {
+
+	public Predicate predicateFindAll(String tuKhoa, Long cha) {
+		BooleanExpression predAll = QCapCoQuanQuanLy.capCoQuanQuanLy.daXoa.eq(false);
+		if (tuKhoa != null && !"".equals(tuKhoa)) {
+			predAll = predAll.and(QCapCoQuanQuanLy.capCoQuanQuanLy.ma.containsIgnoreCase(tuKhoa)
+					.or(QCapCoQuanQuanLy.capCoQuanQuanLy.ten.containsIgnoreCase(tuKhoa))
+					.or(QCapCoQuanQuanLy.capCoQuanQuanLy.moTa.containsIgnoreCase(tuKhoa)));
+		}
+
+		if (cha != null && cha > 0) {
+			predAll = predAll.and(QCapCoQuanQuanLy.capCoQuanQuanLy.cha.id.eq(cha));
+		}
+
+		return predAll;
+	}
+
+	public Predicate predicateFindOne(Long id) {
+		return QCapCoQuanQuanLy.capCoQuanQuanLy.daXoa.eq(false).and(QCapCoQuanQuanLy.capCoQuanQuanLy.id.eq(id));
+	}
+
+	public boolean isExists(CapCoQuanQuanLyRepository repo, Long id) {
+		if (id != null && id > 0) {
+			Predicate predicate = QCapCoQuanQuanLy.capCoQuanQuanLy.daXoa.eq(false)
+					.and(QCapCoQuanQuanLy.capCoQuanQuanLy.id.eq(id));
+			return repo.exists(predicate);
+		}
+		return false;
+	}
+
+	public CapCoQuanQuanLy deleteCapCoQuanQuanLy(CapCoQuanQuanLyRepository repo, Long id) {
+		CapCoQuanQuanLy capCoQuanQuanLy = null;
+		if (isExists(repo, id)) {
+			capCoQuanQuanLy = new CapCoQuanQuanLy();
+			capCoQuanQuanLy.setId(id);
+			capCoQuanQuanLy.setDaXoa(true);
+		}
+		return capCoQuanQuanLy;
+	}
+
+	public boolean checkExistsData(CapCoQuanQuanLyRepository repo, CapCoQuanQuanLy body) {
+		BooleanExpression predAll = QCapCoQuanQuanLy.capCoQuanQuanLy.daXoa.eq(false);
+
+		if (!body.isNew()) {
+			predAll = predAll.and(QCapCoQuanQuanLy.capCoQuanQuanLy.id.ne(body.getId()));
+		}
+
+		predAll = predAll.and(QCapCoQuanQuanLy.capCoQuanQuanLy.ma.eq(body.getMa())
+				.or(QCapCoQuanQuanLy.capCoQuanQuanLy.ten.eq(body.getTen())));
+		CapCoQuanQuanLy capCoQuanQuanLy = repo.findOne(predAll);
+
+		return capCoQuanQuanLy != null ? true : false;
+	}
+
+}
