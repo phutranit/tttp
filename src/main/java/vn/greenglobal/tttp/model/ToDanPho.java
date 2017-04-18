7c4da290b449d5bb713f0d961b2d698fe01215bf
Thanh Ha Xuan <toantt@greenglobal.vn>
2017-04-18 08:10:35 +0700

    feat(TiepCongDan) : them danh sach api tiep cong dan
    them danh sach api tiep cong dan
    
    see #20

--- src/main/java/vn/greenglobal/tttp/model/ToDanPho.java
+++ src/main/java/vn/greenglobal/tttp/model/ToDanPho.java
@@ -15,11 +15,8 @@ import io.swagger.annotations.ApiModelProperty;
 @Table(name = "todanpho")
 @Cache(region = "danhmuc", usage = CacheConcurrencyStrategy.READ_WRITE)
 public class ToDanPho extends Model<ToDanPho> {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5662282127057182748L;
+	
 	@NotNull
 	private String ten = "";
 	private String moTa = "";
--- src/main/java/vn/greenglobal/tttp/service/SoTiepCongDanService.java
+++ src/main/java/vn/greenglobal/tttp/service/SoTiepCongDanService.java
@@ -1,22 +1,13 @@
 package vn.greenglobal.tttp.service;
 
 import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.dsl.BooleanExpression;
 
 import vn.greenglobal.tttp.model.QSoTiepCongDan;
 import vn.greenglobal.tttp.model.SoTiepCongDan;
 import vn.greenglobal.tttp.repository.SoTiepCongDanRepository;
 
 public class SoTiepCongDanService {
-	/*public Predicate predicateFindAll(String ten) {
-		
-		BooleanExpression predAll = QSoTiepCongDan.soTiepCongDan.daXoa.eq(false);
-		if (ten != null && !"".equals(ten)) {
-			predAll = predAll.and(QSoTiepCongDan.soTiepCongDan.ten.eq(ten));
-		}
-		return predAll;
-	}*/
-
+	
 	public Predicate predicateFindOne(Long id) {
 		return QSoTiepCongDan.soTiepCongDan.daXoa.eq(false).and(QSoTiepCongDan.soTiepCongDan.id.eq(id));
 	}
