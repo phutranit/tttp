a0bf16a299f87a1bc91fec721283fac0953fdcb6
thanghq <thanghq@greenglobal.vn>
2017-04-18 15:10:55 +0700

    Them hien tai

--- src/main/java/vn/greenglobal/tttp/controller/DonController.java
+++ src/main/java/vn/greenglobal/tttp/controller/DonController.java
@@ -2,13 +2,25 @@ package vn.greenglobal.tttp.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import vn.greenglobal.core.model.common.BaseController;
 import vn.greenglobal.core.model.common.BaseRepository;
 import vn.greenglobal.tttp.model.Don;
+import vn.greenglobal.tttp.repository.DonRepository;
 import vn.greenglobal.tttp.service.DonService;
 
 @RestController
@@ -17,10 +29,23 @@ import vn.greenglobal.tttp.service.DonService;
 public class DonController extends BaseController<Don> {
 
 	private static Log log = LogFactory.getLog(DonController.class);
-	private static DonService DonService = new DonService();
+	private static DonService donService = new DonService();
+	
+	@Autowired
+	private DonRepository repo;
 	
 	public DonController(BaseRepository<Don, Long> repo) {
 		super(repo);
-		// TODO Auto-generated constructor stub
+
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/dons")
+	@ApiOperation(value = "Lấy danh sách Đơn", position = 1, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody PagedResources<Don> getList(Pageable pageable,
+			PersistentEntityResourceAssembler eass){
+		
+		log.info("Get list 'Don'");
+		Page<Don> pageData =  repo.findAll(donService.predicateFindAll(),pageable);
+		return assembler.toResource(pageData, (ResourceAssembler) eass);
 	}
 }
--- src/main/java/vn/greenglobal/tttp/service/DonService.java
+++ src/main/java/vn/greenglobal/tttp/service/DonService.java
@@ -1,5 +1,31 @@
 package vn.greenglobal.tttp.service;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import vn.greenglobal.tttp.model.QDon;
+
 public class DonService {
 
+	
+//	public Predicate predicateFindAll(String tuKhoa, Long cha) {
+//		BooleanExpression predAll = QThamQuyenGiaiQuyet.thamQuyenGiaiQuyet.daXoa.eq(false);
+//		if (tuKhoa != null && !"".equals(tuKhoa)) {
+//			predAll = predAll.and(QThamQuyenGiaiQuyet.thamQuyenGiaiQuyet.ten.containsIgnoreCase(tuKhoa)
+//					.or(QThamQuyenGiaiQuyet.thamQuyenGiaiQuyet.moTa.containsIgnoreCase(tuKhoa)));
+//		}
+//
+//		if (cha != null && cha > 0) {
+//			predAll = predAll.and(QThamQuyenGiaiQuyet.thamQuyenGiaiQuyet.cha.id.eq(cha));
+//		}
+//
+//		return predAll;
+//	}
+	
+	public Predicate predicateFindAll() {
+		
+		BooleanExpression predAll = QDon.don.daXoa.eq(false);
+		return predAll;
+		
+	}
 }
