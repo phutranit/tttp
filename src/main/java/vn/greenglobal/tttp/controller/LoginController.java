3903203f74254028ff367093dcf82fe30d69a4fb
NamTVD <namtvd@greenglobal.vn>
2017-04-27 14:28:43 +0000

    feat(XuLyDon): update coQuanQuanLyId
    Update coQuanQuanLyId
    
    see #49

--- src/main/java/vn/greenglobal/tttp/controller/LoginController.java
+++ src/main/java/vn/greenglobal/tttp/controller/LoginController.java
@@ -81,7 +81,7 @@ public class LoginController {
 					congChuc = congChucRepository.findOne(congChucService.predicateFindByNguoiDungId(user.getId()));
 					if (congChuc != null) {
 						commonProfile.addAttribute("congChucId", congChuc.getId());
-						commonProfile.addAttribute("coQuanQuanLyID", congChuc.getCoQuanQuanLy() != null ? congChuc.getCoQuanQuanLy().getId() : 0);
+						commonProfile.addAttribute("coQuanQuanLyId", congChuc.getCoQuanQuanLy() != null ? congChuc.getCoQuanQuanLy().getId() : 0);
 					}
 				}
 				String token = generator.generate(commonProfile);
