2ce559f35d63136cd519757be5a03e5ac74c2388
Thanh PX <thanhpx1@greenglobal.vn>
2017-04-27 15:11:27 +0700

    feat (CongDan): lich su thay doi
    them model
    
    see#51

--- src/main/java/vn/greenglobal/tttp/controller/TttpController.java
+++ src/main/java/vn/greenglobal/tttp/controller/TttpController.java
@@ -1,12 +1,19 @@
 package vn.greenglobal.tttp.controller;
 
+import java.util.List;
+
+import javax.persistence.Transient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import vn.greenglobal.core.model.common.BaseController;
 import vn.greenglobal.core.model.common.BaseRepository;
+import vn.greenglobal.tttp.model.PropertyChangeObject;
 import vn.greenglobal.tttp.repository.CongChucRepository;
 import vn.greenglobal.tttp.util.MessageByLocaleService;
 import vn.greenglobal.tttp.util.ProfileUtils;
+
+
 
 public class TttpController<T> extends BaseController<T> {
 
@@ -31,4 +38,20 @@ public class TttpController<T> extends BaseController<T> {
 	public MessageByLocaleService getMessage() {
 		return message;
 	}
+	
+	@Transient
+	public String getChiTietThayDoi(final List<PropertyChangeObject> list) {
+		String out = "";
+		if (list.size() > 0) {
+			out += "[";
+			for (PropertyChangeObject obj : list) {
+				out += "{'propertyName': '" + obj.getPropertyName() + "',"
+						+ "'oldValue': '" + obj.getOldValue() + "','newValue': '" + obj.getNewValue() + "'},";
+			}
+			out +="]";
+			out = out.replaceAll("},]", "}]");
+		}
+		return out;
+	}
+
 }
