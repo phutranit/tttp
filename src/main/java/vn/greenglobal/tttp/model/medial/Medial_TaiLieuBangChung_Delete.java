c673b973913ef4d8ca50c24de2ac37da1d2c593a
namtvd <namtvd@greenglobal.vn>
2017-05-22 15:30:48 +0700

    feat(TaiLieuBangChung): create model medial TaiLieuBangChung
    Create model medial TaiLieuBangChung
    
    see #78

---
+++ src/main/java/vn/greenglobal/tttp/model/medial/Medial_TaiLieuBangChung_Delete.java
@@ -0,0 +1,18 @@
+package vn.greenglobal.tttp.model.medial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Medial_TaiLieuBangChung_Delete {
+
+	List<Medial_TaiLieuBangChung> taiLieuBangChungs = new ArrayList<>();
+
+	public List<Medial_TaiLieuBangChung> getDonCongDans() {
+		return taiLieuBangChungs;
+	}
+
+	public void setDonCongDans(List<Medial_TaiLieuBangChung> taiLieuBangChungs) {
+		this.taiLieuBangChungs = taiLieuBangChungs;
+	}
+
+}
