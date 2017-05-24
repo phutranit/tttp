6a6dfcf27df709342ca9dd1965c09608b7ca83fa
namtvd <namtvd@greenglobal.vn>
2017-05-24 14:14:00 +0700

    feat(LinhVucDonThu): update enum loại đơn
    Update enum loại đơn
    
    see #80

--- src/main/java/vn/greenglobal/tttp/enums/LoaiDonEnum.java
+++ src/main/java/vn/greenglobal/tttp/enums/LoaiDonEnum.java
@@ -5,6 +5,7 @@ public enum LoaiDonEnum {
 	DON_KHIEU_NAI("Khiếu nại"),
 	DON_TO_CAO("Tố cáo"),
 	DON_KIEN_NGHI_PHAN_ANH("Kiến nghị, phản ánh"),
+	DON_TRANH_CHAP_DAT("Tranh chấp đất"),
 	DON_CO_NHIEU_NOI_DUNG_KHAC_NHAU("Đơn có nhiều nội dung khác nhau");
 
 	LoaiDonEnum(String text) {
