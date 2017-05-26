0b4dc994917c7c4185ee71bb4efcfd9ae6af2e61
namtvd <namtvd@greenglobal.vn>
2017-05-26 09:50:52 +0700

    feat(GiaiQuyetDon): create repo and service LichSuGiaiQuyetDon
    Create repo and service LichSuGiaiQuyetDon
    
    see #83

--- src/main/java/vn/greenglobal/tttp/repository/GiaiQuyetDonRepository.java
+++ src/main/java/vn/greenglobal/tttp/repository/GiaiQuyetDonRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import vn.greenglobal.core.model.common.MutableRepo;
 import vn.greenglobal.tttp.model.GiaiQuyetDon;
-import vn.greenglobal.tttp.model.XuLyDon;
 
 @RestResource(path = "giaiQuyetDons")
 public interface GiaiQuyetDonRepository extends MutableRepo<GiaiQuyetDon> {
