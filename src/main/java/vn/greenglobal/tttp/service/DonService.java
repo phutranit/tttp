587dfb2eb395e9f816cb792c23a04e3606d7d8a1
Thanh PX <thanhpx1@greenglobal.vn>
2017-06-02 08:47:31 +0700

    remove sysout

--- src/main/java/vn/greenglobal/tttp/service/DonService.java
+++ src/main/java/vn/greenglobal/tttp/service/DonService.java
@@ -229,7 +229,6 @@ public class DonService {
 		OrderSpecifier<Integer> sortOrder = QGiaiQuyetDon.giaiQuyetDon.thuTuThucHien.desc();		
 		Collection<GiaiQuyetDon> giaiQuyetDons = (Collection<GiaiQuyetDon>) giaiQuyetDonRepo.findAll(giaiQuyetDonQuery, sortOrder);
 		donCollections = giaiQuyetDons.stream().map(d -> d.getThongTinGiaiQuyetDon().getDon()).distinct().collect(Collectors.toList());
-		System.out.println("donCollections: " + donCollections.size());
 		predAll = predAll.and(QDon.don.in(donCollections));
 		return predAll;
 	}
