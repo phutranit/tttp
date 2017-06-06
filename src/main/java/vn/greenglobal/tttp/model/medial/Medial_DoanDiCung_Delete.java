a88321cf981422409c7b14fd72477a7163395273
namtvd <namtvd@greenglobal.vn>
2017-06-06 14:19:07 +0700

    feat(DoanDiCung): create model DoanDiCung medial
    Create model DoanDiCung medial
    
    see #106

---
+++ src/main/java/vn/greenglobal/tttp/model/medial/Medial_DoanDiCung_Delete.java
@@ -0,0 +1,18 @@
+package vn.greenglobal.tttp.model.medial;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Medial_DoanDiCung_Delete {
+
+	List<Medial_DoanDiCung> doanDiCungs = new ArrayList<>();
+
+	public List<Medial_DoanDiCung> getDoanDiCungs() {
+		return doanDiCungs;
+	}
+
+	public void setDoanDiCungs(List<Medial_DoanDiCung> doanDiCungs) {
+		this.doanDiCungs = doanDiCungs;
+	}
+
+}
