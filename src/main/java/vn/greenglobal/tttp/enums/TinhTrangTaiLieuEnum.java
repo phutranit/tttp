a9cbc9ecef180761b010a7a3ca1f7024a196cb04
namtvd <namtvd@greenglobal.vn>
2017-06-07 10:53:25 +0700

    feat(TaiLieuBangChung): update enum tai lieu bang chung
    Update enum tai lieu bang chung
    
    see #107

--- src/main/java/vn/greenglobal/tttp/enums/TinhTrangTaiLieuEnum.java
+++ src/main/java/vn/greenglobal/tttp/enums/TinhTrangTaiLieuEnum.java
@@ -3,8 +3,8 @@ package vn.greenglobal.tttp.enums;
 public enum TinhTrangTaiLieuEnum {
 
 	BAN_GOC("Bản gốc"),
-	BAN_SAO("Bảo sao"),
-	BAN_SAO_CONG_CHUNG("Bảo sao công chứng");
+	BAN_SAO("Bản sao"),
+	BAN_SAO_CONG_CHUNG("Bản sao công chứng");
 
 	TinhTrangTaiLieuEnum(String text) {
 		this.text = text;
