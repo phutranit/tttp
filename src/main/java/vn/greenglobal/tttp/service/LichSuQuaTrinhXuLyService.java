ec1533bffb87b85a3e0d12a4d97cf0111ebcb7e7
Thanh Ha Xuan <toantt@greenglobal.vn>
2017-06-13 09:35:44 +0700

    fix (Xu Ly Don) : fix xu ly don
    fix thong tin thoi han, lich su xu ly don
    
    see #123

--- src/main/java/vn/greenglobal/tttp/service/LichSuQuaTrinhXuLyService.java
+++ src/main/java/vn/greenglobal/tttp/service/LichSuQuaTrinhXuLyService.java
@@ -27,9 +27,18 @@ public class LichSuQuaTrinhXuLyService {
 		return predAll;
 	}
 	
-	public int timThuTuLichSuQuaTrinhXuLyHienTai(LichSuQuaTrinhXuLyRepository repo, Long donId) {
+	/*public int timThuTuLichSuQuaTrinhXuLyHienTai(LichSuQuaTrinhXuLyRepository repo, Long donId) {
 		int thuTu = 0;
 		List<LichSuQuaTrinhXuLy> lichSuList = (List<LichSuQuaTrinhXuLy>) repo.findAll(predicateFindAll(donId));
+		if (lichSuList != null) {
+			thuTu = lichSuList.size();
+		}
+		return thuTu;
+	}*/
+	
+	public int timThuTuLichSuQuaTrinhXuLyHienTai(LichSuQuaTrinhXuLyRepository repo, Long donId, Long donViId) {
+		int thuTu = 0;
+		List<LichSuQuaTrinhXuLy> lichSuList = (List<LichSuQuaTrinhXuLy>) repo.findAll(predicateFindAll(donId, donViId));
 		if (lichSuList != null) {
 			thuTu = lichSuList.size();
 		}
