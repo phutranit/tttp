ffc5a9a663fc536da3d2c46689a2b02311115486
Thanh Ha Xuan <toantt@greenglobal.vn>
2017-06-15 17:42:39 +0700

    feat (Workow trang thai don vi) :  : api trang thai don vi
    tao api them sua xoa trang thai don vi
    
    see #145

--- src/main/java/vn/greenglobal/tttp/controller/DonViHasStateController.java
+++ src/main/java/vn/greenglobal/tttp/controller/DonViHasStateController.java
@@ -1,6 +1,5 @@
 package vn.greenglobal.tttp.controller;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,94 +25,90 @@ import io.swagger.annotations.ApiResponses;
 import vn.greenglobal.core.model.common.BaseRepository;
 import vn.greenglobal.tttp.enums.ApiErrorEnum;
 import vn.greenglobal.tttp.enums.QuyenEnum;
-import vn.greenglobal.tttp.model.Form;
-import vn.greenglobal.tttp.repository.FormRepository;
-import vn.greenglobal.tttp.repository.TransitionRepository;
-import vn.greenglobal.tttp.service.FormService;
+import vn.greenglobal.tttp.model.DonViHasState;
+import vn.greenglobal.tttp.repository.DonViHasStateRepository;
+import vn.greenglobal.tttp.service.DonViHasStateService;
 import vn.greenglobal.tttp.util.Utils;
 
 @RestController
 @RepositoryRestController
-@Api(value = "forms", description = "Form")
-public class FormController extends TttpController<Form> {
+@Api(value = "donViHasStates", description = "Danh sách trạng thái của đơn vị")
+public class DonViHasStateController extends TttpController<DonViHasState> {
 
 	@Autowired
-	private FormRepository formRepo;
+	private DonViHasStateRepository donViHasStateRepo;
 	
 	@Autowired
-	private FormService formService;
+	private DonViHasStateService donViHasStateService;
 	
-	@Autowired
-	private TransitionRepository transitionRepo;
-	
-	public FormController(BaseRepository<Form, Long> repo) {
+	public DonViHasStateController(BaseRepository<DonViHasState, Long> repo) {
 		super(repo);
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@RequestMapping(method = RequestMethod.GET, value = "/forms")
-	@ApiOperation(value = "Lấy danh sách Form", position = 1, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET, value = "/donViHasStates")
+	@ApiOperation(value = "Lấy danh sách trạng thái của đơn vị", position = 1, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Object getList(@RequestHeader(value = "Authorization", required = true) String authorization,
-			Pageable pageable, @RequestParam(value = "tuKhoa", required = false) String tuKhoa,
-			 @RequestParam(value = "processType", required = false) String processType,
+			Pageable pageable, @RequestParam(value = "processType", required = false) String processType,
 			PersistentEntityResourceAssembler eass) {
 
 		try {
-			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_LIETKE) == null
-					&& Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_XEM) == null) {
+			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_LIETKE) == null
+					&& Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_XEM) == null) {
 				return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 						ApiErrorEnum.ROLE_FORBIDDEN.getText());
 			}
 
-			Page<Form> page = formRepo.findAll(formService.predicateFindAll(tuKhoa, processType), pageable);
+			Page<DonViHasState> page = donViHasStateRepo.findAll(donViHasStateService.predicateFindAll(processType), pageable);
 			return assembler.toResource(page, (ResourceAssembler) eass);
 		} catch (Exception e) {
 			return Utils.responseInternalServerErrors();
 		}
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/forms/{id}")
-	@ApiOperation(value = "Lấy Form theo Id", position = 3, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Lấy Form thành công", response = Form.class) })
+	@RequestMapping(method = RequestMethod.GET, value = "/donViHasStates/{id}")
+	@ApiOperation(value = "Lấy Trạng thái của đơn vị theo Id", position = 3, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Lấy Trạng thái của đơn vị thành công", response = DonViHasState.class) })
 	public ResponseEntity<Object> getById(@RequestHeader(value = "Authorization", required = true) String authorization,
 			@PathVariable("id") long id, PersistentEntityResourceAssembler eass) {
 
 		try {
-			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_XEM) == null) {
+			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_XEM) == null) {
 				return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 						ApiErrorEnum.ROLE_FORBIDDEN.getText());
 			}
 
-			Form form = formRepo.findOne(formService.predicateFindOne(id));
-			if (form == null) {
+			DonViHasState donViHasState = donViHasStateRepo.findOne(donViHasStateService.predicateFindOne(id));
+			if (donViHasState == null) {
 				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 			}
 
-			return new ResponseEntity<>(eass.toFullResource(form), HttpStatus.OK);
+			return new ResponseEntity<>(eass.toFullResource(donViHasState), HttpStatus.OK);
 		} catch (Exception e) {
 			return Utils.responseInternalServerErrors();
 		}
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/forms")
-	@ApiOperation(value = "Thêm mới Form", position = 2, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.POST, value = "/donViHasStates")
+	@ApiOperation(value = "Thêm mới trạng thái của đơn vị", position = 2, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Thêm mới Form thành công", response = Form.class),
-			@ApiResponse(code = 201, message = "Thêm mới Form thành công", response = Form.class) })
+			@ApiResponse(code = 200, message = "Thêm mới trạng thái của đơn vị thành công", response = DonViHasState.class),
+			@ApiResponse(code = 201, message = "Thêm mới trạng thái của đơn vị thành công", response = DonViHasState.class) })
 	public ResponseEntity<Object> create(@RequestHeader(value = "Authorization", required = true) String authorization,
-			@RequestBody Form form, PersistentEntityResourceAssembler eass) {
+			@RequestBody DonViHasState donViHasState, PersistentEntityResourceAssembler eass) {
 
 		try {
-			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_THEM) == null) {
+			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_THEM) == null) {
 				return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 						ApiErrorEnum.ROLE_FORBIDDEN.getText());
 			}
 
-			if (StringUtils.isNotBlank(form.getTen()) && formService.checkExistsData(formRepo, form)) {
+			if (donViHasStateService.checkExistsData(donViHasStateRepo, donViHasState)) {
 				return Utils.responseErrors(HttpStatus.BAD_REQUEST, ApiErrorEnum.TEN_EXISTS.name(),
 						ApiErrorEnum.TEN_EXISTS.getText());
 			}
-			return Utils.doSave(formRepo, form,
+			
+			return Utils.doSave(donViHasStateRepo, donViHasState,
 					Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("congChucId").toString()), eass,
 					HttpStatus.CREATED);
 		} catch (Exception e) {
@@ -121,32 +116,32 @@ public class FormController extends TttpController<Form> {
 		}
 	}
 	
-	@RequestMapping(method = RequestMethod.PATCH, value = "/forms/{id}")
-	@ApiOperation(value = "Cập nhật Form", position = 4, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.PATCH, value = "/donViHasStates/{id}")
+	@ApiOperation(value = "Cập nhật trạng thái của đơn vị", position = 4, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Cập nhật Form thành công", response = Form.class) })
+			@ApiResponse(code = 200, message = "Cập nhật trạng thái của đơn vị thành công", response = DonViHasState.class) })
 	public @ResponseBody ResponseEntity<Object> update(
 			@RequestHeader(value = "Authorization", required = true) String authorization, @PathVariable("id") long id,
-			@RequestBody Form form, PersistentEntityResourceAssembler eass) {
+			@RequestBody DonViHasState donViHasState, PersistentEntityResourceAssembler eass) {
 
 		try {
-			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_SUA) == null) {
+			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_SUA) == null) {
 				return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 						ApiErrorEnum.ROLE_FORBIDDEN.getText());
 			}
 
-			form.setId(id);
-			if (StringUtils.isNotBlank(form.getTen()) && formService.checkExistsData(formRepo, form)) {
+			donViHasState.setId(id);
+			if (donViHasStateService.checkExistsData(donViHasStateRepo, donViHasState)) {
 				return Utils.responseErrors(HttpStatus.BAD_REQUEST, ApiErrorEnum.TEN_EXISTS.name(),
 						ApiErrorEnum.TEN_EXISTS.getText());
 			}
 
-			if (!formService.isExists(formRepo, id)) {
+			if (!donViHasStateService.isExists(donViHasStateRepo, id)) {
 				return Utils.responseErrors(HttpStatus.NOT_FOUND, ApiErrorEnum.DATA_NOT_FOUND.name(),
 						ApiErrorEnum.DATA_NOT_FOUND.getText());
 			}
 
-			return Utils.doSave(formRepo, form,
+			return Utils.doSave(donViHasStateRepo, donViHasState,
 					Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("congChucId").toString()), eass,
 					HttpStatus.OK);
 		} catch (Exception e) {
@@ -154,30 +149,25 @@ public class FormController extends TttpController<Form> {
 		}
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/forms/{id}")
-	@ApiOperation(value = "Xoá Form", position = 5, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiResponses(value = { @ApiResponse(code = 204, message = "Xoá Form thành công") })
+	@RequestMapping(method = RequestMethod.DELETE, value = "/donViHasStates/{id}")
+	@ApiOperation(value = "Xoá trạng thái của đơn vị", position = 5, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiResponses(value = { @ApiResponse(code = 204, message = "Xoá trạng thái của đơn vị thành công") })
 	public ResponseEntity<Object> delete(@RequestHeader(value = "Authorization", required = true) String authorization,
 			@PathVariable("id") Long id) {
 
 		try {
-			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.FORM_XOA) == null) {
+			if (Utils.quyenValidate(profileUtil, authorization, QuyenEnum.DONVIHASSTATE_XOA) == null) {
 				return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 						ApiErrorEnum.ROLE_FORBIDDEN.getText());
 			}
 
-			if (formService.checkUsedData(transitionRepo, id)) {
-				return Utils.responseErrors(HttpStatus.BAD_REQUEST, ApiErrorEnum.DATA_USED.name(),
-						ApiErrorEnum.DATA_USED.getText());
-			}
-
-			Form form = formService.delete(formRepo, id);
-			if (form == null) {
+			DonViHasState donViHasState = donViHasStateService.delete(donViHasStateRepo, id);
+			if (donViHasState == null) {
 				return Utils.responseErrors(HttpStatus.NOT_FOUND, ApiErrorEnum.DATA_NOT_FOUND.name(),
 						ApiErrorEnum.DATA_NOT_FOUND.getText());
 			}
 
-			Utils.save(formRepo, form,
+			Utils.save(donViHasStateRepo, donViHasState,
 					Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("congChucId").toString()));
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		} catch (Exception e) {
