1e08249d610d4ad668f2c4c1efd45b5e475f974b
Thanh Ha Xuan <toantt@greenglobal.vn>
2017-07-07 17:27:12 +0700

    fix (xu ly don) : fix xu ly don
    fix gio
    
    see #192

--- src/main/java/vn/greenglobal/tttp/util/Utils.java
+++ src/main/java/vn/greenglobal/tttp/util/Utils.java
@@ -436,7 +436,7 @@ public class Utils {
 					gio = 4;
 				}
 			}
-			str = (gio < 0 ? 0 : "0" + gio) + ":" + (String.valueOf(phut).length() == 1 ? "0" + phut : phut);
+			str = ((String.valueOf(gio).length() == 1 ? "0" + gio : gio)) + ":" + (String.valueOf(phut).length() == 1 ? "0" + phut : phut);
 		}
 		return str;
 	}
