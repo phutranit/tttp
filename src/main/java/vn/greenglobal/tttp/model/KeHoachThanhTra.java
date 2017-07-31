5fd5cfbb43ce692adceddb0b2d0fdb74d559f7a3
namtvd <namtvd@greenglobal.vn>
2017-07-31 08:08:15 +0700

    Update model ke hoach thanh tra

--- src/main/java/vn/greenglobal/tttp/model/KeHoachThanhTra.java
+++ src/main/java/vn/greenglobal/tttp/model/KeHoachThanhTra.java
@@ -1,5 +1,7 @@
 package vn.greenglobal.tttp.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -15,6 +17,9 @@ public class KeHoachThanhTra extends Model<KeHoachThanhTra> {
 	 */
 	private static final long serialVersionUID = -4128391955224263426L;
 	
+	private String soQuyetDinh = "";
+	private String kyThanhTra = "";
 	
+	private LocalDateTime ngayRaQuyetDinh;
 
 }
