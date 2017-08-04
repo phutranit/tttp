5f0ac2eda4837d0abb7a1f8b5d50e4e865eb5294
namtvd <namtvd@greenglobal.vn>
2017-08-04 08:57:16 +0700

    feat(LinhVucDoiTuongThanhTra): api linh vuc doi tuong thanh tra
    Api linh vuc doi tuong thanh tra
    
    see 246

--- src/main/java/vn/greenglobal/tttp/controller/QuyenController.java
+++ src/main/java/vn/greenglobal/tttp/controller/QuyenController.java
@@ -29,6 +29,12 @@ public class QuyenController {
 		List<Map<String, Object>> quyenList = new ArrayList<>();
 		Map<String, Object> quyenObj = new HashMap<>();
 		
+		quyenObj.put("tenChucNang","Lĩnh vực đối tượng thanh tra");
+		quyenObj.put("tenQuyen", QuyenEnum.LINHVUCDOITUONGTHANHTRA_LIETKE.getText() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_XEM.getText() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_THEM.getText() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_SUA.getText() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_XOA.getText());
+		quyenObj.put("giaTriQuyen", QuyenEnum.LINHVUCDOITUONGTHANHTRA_LIETKE.name() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_XEM.name() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_THEM.name() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_SUA.name() + "," + QuyenEnum.LINHVUCDOITUONGTHANHTRA_XOA.name());
+		quyenList.add(quyenObj);
+		
+		quyenObj = new HashMap<>();
 		quyenObj.put("tenChucNang","Thẩm quyền giải quyết");
 		quyenObj.put("tenQuyen", QuyenEnum.THAMQUYENGIAIQUYET_LIETKE.getText() + "," + QuyenEnum.THAMQUYENGIAIQUYET_XEM.getText() + "," + QuyenEnum.THAMQUYENGIAIQUYET_THEM.getText() + "," + QuyenEnum.THAMQUYENGIAIQUYET_SUA.getText() + "," + QuyenEnum.THAMQUYENGIAIQUYET_XOA.getText());
 		quyenObj.put("giaTriQuyen", QuyenEnum.THAMQUYENGIAIQUYET_LIETKE.name() + "," + QuyenEnum.THAMQUYENGIAIQUYET_XEM.name() + "," + QuyenEnum.THAMQUYENGIAIQUYET_THEM.name() + "," + QuyenEnum.THAMQUYENGIAIQUYET_SUA.name() + "," + QuyenEnum.THAMQUYENGIAIQUYET_XOA.name());
---
+++ src/main/java/vn/greenglobal/tttp/service/LinhVucDoiTuongThanhTraService.java
@@ -0,0 +1,93 @@
+package vn.greenglobal.tttp.service;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import vn.greenglobal.tttp.model.LinhVucDoiTuongThanhTra;
+import vn.greenglobal.tttp.model.QLinhVucDoiTuongThanhTra;
+import vn.greenglobal.tttp.repository.LinhVucDoiTuongThanhTraRepository;
+import vn.greenglobal.tttp.util.Utils;
+
+@Component
+public class LinhVucDoiTuongThanhTraService {
+	
+	@Autowired
+	private LinhVucDoiTuongThanhTraRepository linhVucDoiTuongThanhTraRepository;
+
+	BooleanExpression base = QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.daXoa.eq(false);
+	
+	public Predicate predicateFindAll(String tuKhoa) {
+		BooleanExpression predAll = base;
+		if (tuKhoa != null && StringUtils.isNotBlank(tuKhoa.trim())) {
+			predAll = predAll.and(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.ten.containsIgnoreCase(tuKhoa.trim())
+					.or(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.moTa.containsIgnoreCase(tuKhoa.trim())));
+		}
+
+		return predAll;
+	}
+
+	public Predicate predicateFindOne(Long id) {
+		return base.and(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.id.eq(id));
+	}
+
+	public boolean isExists(LinhVucDoiTuongThanhTraRepository repo, Long id) {
+		if (id != null && id > 0) {
+			Predicate predicate = base.and(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.id.eq(id));
+			return repo.exists(predicate);
+		}
+		return false;
+	}
+
+	public LinhVucDoiTuongThanhTra delete(LinhVucDoiTuongThanhTraRepository repo, Long id) {
+		LinhVucDoiTuongThanhTra linhVucDoiTuongThanhTra = repo.findOne(predicateFindOne(id));
+
+		if (linhVucDoiTuongThanhTra != null) {
+			linhVucDoiTuongThanhTra.setDaXoa(true);
+		}
+
+		return linhVucDoiTuongThanhTra;
+	}
+
+	public boolean checkExistsData(LinhVucDoiTuongThanhTraRepository repo, LinhVucDoiTuongThanhTra body) {
+		BooleanExpression predAll = base;
+
+		if (!body.isNew()) {
+			predAll = predAll.and(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.id.ne(body.getId()));
+		}
+
+		predAll = predAll.and(QLinhVucDoiTuongThanhTra.linhVucDoiTuongThanhTra.ten.eq(body.getTen()));
+		List<LinhVucDoiTuongThanhTra> linhVucDoiTuongThanhTras = (List<LinhVucDoiTuongThanhTra>) repo.findAll(predAll);
+
+		return linhVucDoiTuongThanhTras != null && linhVucDoiTuongThanhTras.size() > 0 ? true : false;
+	}
+
+//	public boolean checkUsedData(DonRepository donRepository, Long id) {
+//		List<Don> donList = (List<Don>) donRepository.findAll(QDon.don.daXoa.eq(false)
+//				.and(QDon.don.linhVucDonThu.id.eq(id)).or(QDon.don.linhVucDonThuChiTiet.id.eq(id))
+//				.or(QDon.don.chiTietLinhVucDonThuChiTiet.id.eq(id)));
+//
+//		if ((linhVucDonThuList != null && linhVucDonThuList.size() > 0) || (donList != null && donList.size() > 0)
+//				|| (donList != null && donList.size() > 0)) {
+//			return true;
+//		}
+//
+//		return false;
+//	}
+	
+	public LinhVucDoiTuongThanhTra save(LinhVucDoiTuongThanhTra obj, Long congChucId) {
+		return Utils.save(linhVucDoiTuongThanhTraRepository, obj, congChucId);
+	}
+	
+	public ResponseEntity<Object> doSave(LinhVucDoiTuongThanhTra obj, Long congChucId, PersistentEntityResourceAssembler eass, HttpStatus status) {
+		return Utils.doSave(linhVucDoiTuongThanhTraRepository, obj, congChucId, eass, status);		
+	}
+}
