5f4d40b51041d72ce4843bcc4e7c81b60e0a8e82
Thanh Ha Xuan <toantt@greenglobal.vn>
2017-08-07 13:22:56 +0700

    fix (loai doi tuong) : fix loai doi tuong
    fix require loaiDon
    
    see #251

--- src/main/java/vn/greenglobal/tttp/controller/EnumController.java
+++ src/main/java/vn/greenglobal/tttp/controller/EnumController.java
@@ -170,7 +170,7 @@ public class EnumController {
 	@ApiOperation(value = "Lấy danh sách Loại Đối Tượng", position = 3, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ResponseEntity<Object> getLoaiDoiTuongs(
 			@RequestHeader(value = "Authorization", required = true) String authorization,
-			@RequestHeader(value = "loaiDonThu", required = true) String loaiDonThu) {
+			@RequestHeader(value = "loaiDonThu", required = false) String loaiDonThu) {
 		List<Map<String, Object>> list = new ArrayList<>();
 		Map<String, Object> object = new HashMap<>();
 		
