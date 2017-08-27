050dcafa7096cc4b9b4491d586a99e58ae47480e
namtvd <namtvd@greenglobal.vn>
2017-08-27 15:44:34 +0700

    faet(CuocThanhTra): api cuoc thanh tra
    Api cuoc thanh tra
    
    see #273

---
+++ src/main/java/vn/greenglobal/tttp/model/medial/Medial_CuocThanhTra.java
@@ -0,0 +1,9 @@
+package vn.greenglobal.tttp.model.medial;
+
+import vn.greenglobal.tttp.model.Model;
+
+@SuppressWarnings("serial")
+public class Medial_CuocThanhTra extends Model<Medial_CuocThanhTra> {
+
+
+}
