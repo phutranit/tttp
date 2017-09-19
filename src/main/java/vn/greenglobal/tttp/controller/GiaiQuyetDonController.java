af09170dd838126565c100d5157ac205b3b6ba0a
namtvd <namtvd@greenglobal.vn>
2017-09-19 10:00:16 +0700

    fix(GiaiQuyetDon): fix noi dung y kien giai quyet
    see #302

--- src/main/java/vn/greenglobal/tttp/controller/GiaiQuyetDonController.java
+++ src/main/java/vn/greenglobal/tttp/controller/GiaiQuyetDonController.java
@@ -813,6 +813,7 @@ public class GiaiQuyetDonController extends TttpController<GiaiQuyetDon> {
 			disableGiaiQuyetDonCu(VaiTroEnum.TRUONG_PHONG, donId, congChuc);
 			giaiQuyetDonTiepTheo.setChucVu(VaiTroEnum.TRUONG_PHONG);
 			giaiQuyetDonTiepTheo.setThuTuThucHien(giaiQuyetDonHienTai.getThuTuThucHien() + 1);
+			giaiQuyetDonTiepTheo.setyKienGiaiQuyet(giaiQuyetDon.getyKienGiaiQuyet());
 			if (giaiQuyetDon.getCanBoXuLyChiDinh() != null) {
 				giaiQuyetDonTiepTheo.setCanBoXuLyChiDinh(giaiQuyetDon.getCanBoXuLyChiDinh());
 			}
@@ -826,7 +827,7 @@ public class GiaiQuyetDonController extends TttpController<GiaiQuyetDon> {
 			giaiQuyetDonTruongPhong.setPhongBanGiaiQuyet(giaiQuyetDon.getPhongBanGiaiQuyet());
 			giaiQuyetDonTruongPhong.setThongTinGiaiQuyetDon(giaiQuyetDon.getThongTinGiaiQuyetDon());
 			giaiQuyetDonTruongPhong.setDonViGiaiQuyet(giaiQuyetDonHienTai.getDonViGiaiQuyet());
-			//giaiQuyetDonTruongPhong.setyKienGiaiQuyet(giaiQuyetDon.getyKienGiaiQuyet());
+			giaiQuyetDonTruongPhong.setyKienGiaiQuyet(giaiQuyetDon.getyKienGiaiQuyet());
 			giaiQuyetDonTruongPhong.setDonViChuyenDon(giaiQuyetDonHienTai.getDonViChuyenDon());
 			giaiQuyetDonTruongPhong.setLaTTXM(isLaTTXM);
 			if (isLaTTXM) { 
@@ -873,7 +874,7 @@ public class GiaiQuyetDonController extends TttpController<GiaiQuyetDon> {
 		giaiQuyetDonTiepTheo.setPhongBanGiaiQuyet(giaiQuyetDon.getPhongBanGiaiQuyet());
 		giaiQuyetDonTiepTheo.setThongTinGiaiQuyetDon(giaiQuyetDon.getThongTinGiaiQuyetDon());
 		giaiQuyetDonTiepTheo.setDonViGiaiQuyet(giaiQuyetDonHienTai.getDonViGiaiQuyet());
-//		giaiQuyetDonTiepTheo.setyKienGiaiQuyet(giaiQuyetDon.getyKienGiaiQuyet());
+		giaiQuyetDonTiepTheo.setyKienGiaiQuyet(giaiQuyetDon.getyKienGiaiQuyet());
 		giaiQuyetDonTiepTheo.setDonViChuyenDon(giaiQuyetDonHienTai.getDonViChuyenDon());
 		giaiQuyetDonTiepTheo.setLaTTXM(isLaTTXM);
 		if (isLaTTXM) { 
@@ -914,8 +915,11 @@ public class GiaiQuyetDonController extends TttpController<GiaiQuyetDon> {
 		}
 		
 		donService.save(don, congChucId);
+		System.out.println("Nam1: " + giaiQuyetDonHienTai.getId() + " --- " + giaiQuyetDonHienTai.getyKienGiaiQuyet());
 		giaiQuyetDonService.save(giaiQuyetDonHienTai, congChucId);
+		System.out.println("Nam2: " + giaiQuyetDonHienTai.getId() + " --- " + giaiQuyetDonHienTai.getyKienGiaiQuyet());
 		giaiQuyetDonService.save(giaiQuyetDonTruongPhong, congChucId);
+		System.out.println("Nam3: " + giaiQuyetDonHienTai.getId() + " --- " + giaiQuyetDonHienTai.getyKienGiaiQuyet());
 		
 		//tao lich su qua trinh xu ly don
 		//State state = stateRepo.findOne(giaiQuyetDonHienTai.getNextState().getId());
