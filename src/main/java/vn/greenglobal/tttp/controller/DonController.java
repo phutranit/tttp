aebeb61d9c3bda368f432907eaae30c72063fb94
Thanh PX <thanhpx1@greenglobal.vn>
2017-11-24 13:31:15 +0700

    feat (TraCuuDon): can bo
    cap nhat medial
    
    see#400

--- src/main/java/vn/greenglobal/tttp/controller/DonController.java
+++ src/main/java/vn/greenglobal/tttp/controller/DonController.java
@@ -164,6 +164,9 @@ public class DonController extends TttpController<Don> {
 	protected PagedResourcesAssembler<LichSuThayDoi> lichSuThayDoiAssembler;
 	
 	@Autowired
+	protected PagedResourcesAssembler<Medial_DonTraCuu> mediaTraCuuDonAssembler;
+	
+	@Autowired
 	private ThamSoRepository thamSoRepository;
 	
 	@Autowired
@@ -472,7 +475,7 @@ public class DonController extends TttpController<Don> {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@RequestMapping(method = RequestMethod.GET, value = "/traCuuDons/canBo")
 	@ApiOperation(value = "Lấy danh sách Đơn", position = 1, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Lấy dữ liệu đơn thành công", response = Don.class),
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Lấy dữ liệu đơn thành công", response = Medial_DonTraCuu.class),
 			@ApiResponse(code = 203, message = "Không có quyền lấy dữ liệu"),
 			@ApiResponse(code = 204, message = "Không có dữ liệu"),
 			@ApiResponse(code = 400, message = "Param không đúng kiểu"), })
@@ -481,90 +484,45 @@ public class DonController extends TttpController<Don> {
 			@RequestParam(value = "tuKhoa", required = false) String tuKhoa,
 			@RequestParam(value = "nguonDon", required = false) String nguonDon,
 			@RequestParam(value = "phanLoaiDon", required = false) String phanLoaiDon,
-			@RequestParam(value = "tiepNhanTuNgay", required = false) String tiepNhanTuNgay,
-			@RequestParam(value = "tiepNhanDenNgay", required = false) String tiepNhanDenNgay,
-			@RequestParam(value = "hanGiaiQuyetTuNgay", required = false) String hanGiaiQuyetTuNgay,
-			@RequestParam(value = "hanGiaiQuyetDenNgay", required = false) String hanGiaiQuyetDenNgay,
+			@RequestParam(value = "tuNgay", required = false) String tuNgay,
+			@RequestParam(value = "denNgay", required = false) String denNgay,
 			@RequestParam(value = "tinhTrangXuLy", required = false) String tinhTrangXuLy,
-			@RequestParam(value = "thanhLapDon", required = true) boolean thanhLapDon,
+			@RequestParam(value = "linhVucId", required = false) Long linhVucId,
+			@RequestParam(value = "linhVucChiTietId", required = false) Long linhVucChiTietId,
 			@RequestParam(value = "trangThaiDon", required = false) String trangThaiDon,
-			@RequestParam(value = "phongBanGiaiQuyetXLD", required = false) Long phongBanGiaiQuyet,
-			@RequestParam(value = "coQuanTiepNhanXLD", required = false) Long coQuanTiepNhanXLD,
 			@RequestParam(value = "hoTen", required = false) String hoTen, 
 			@RequestParam(value = "trangThaiDonToanHT", required = false) String trangThaiDonToanHT,
 			@RequestParam(value = "ketQuaToanHT", required = false) String ketQuaToanHT,
+			@RequestParam(value = "taiDonVi", required = false) boolean taiDonVi,
+			@RequestParam(value = "listDonViTiepNhan", required = false) List<CoQuanQuanLy> listDonViTiepNhan,
 			PersistentEntityResourceAssembler eass) {
 
 		try {
 
 			NguoiDung nguoiDung = Utils.quyenValidate(profileUtil, authorization, QuyenEnum.XULYDON_LIETKE);
 			if (nguoiDung != null) {
-				boolean coQuyTrinh = true;
 				Long donViXuLyXLD = Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("donViId").toString());
-				Long phongBanXuLyXLD = Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("coQuanQuanLyId").toString());
-				String vaiTroNguoiDungHienTai = profileUtil.getCommonProfile(authorization).getAttribute("loaiVaiTro").toString();
-				Long canBoXuLyXLD = Long.valueOf(profileUtil.getCommonProfile(authorization).getAttribute("congChucId").toString());
-				CongChuc congChuc = congChucRepo.findOne(canBoXuLyXLD);
-				if (StringUtils.equals(VaiTroEnum.LANH_DAO.name(), vaiTroNguoiDungHienTai) 
-						|| StringUtils.equals(VaiTroEnum.VAN_THU.name(), vaiTroNguoiDungHienTai)) {
-					phongBanXuLyXLD = 0L;
-				}
-				
-				NumberExpression<Integer> canBoXuLyChiDinh = null;
 				OrderSpecifier<LocalDateTime> sortOrderDon = null;
-				OrderSpecifier<Integer> sortOrderDonByCanBo = null;
 				List<Don> listDon = new ArrayList<Don>();
 				
-				State beginState = repoState.findOne(serviceState.predicateFindByType(FlowStateEnum.BAT_DAU));
-				Predicate predicateProcess = processService.predicateFindAllByDonVi(coQuanQuanLyRepo.findOne(donViXuLyXLD), ProcessTypeEnum.XU_LY_DON);
-				List<Process> listProcess = (List<Process>) repoProcess.findAll(predicateProcess);
-				List<State> listState = new ArrayList<State>();
-				List<Process> listProcessHaveBeginState = new ArrayList<Process>();
-				for (Process processFromList : listProcess) {
-					Predicate predicate = serviceState.predicateFindAll(beginState.getId(), processFromList, repoTransition);
-					listState = ((List<State>) repoState.findAll(predicate));
-					if (listState.size() > 0) {
-						State state = listState.get(0);
-						if (!state.getType().equals(FlowStateEnum.KET_THUC)) {								
-							listProcessHaveBeginState.add(processFromList);
-							break;
-						}						
-					}
-				}
-				if (listProcessHaveBeginState.size() < 1) {
-					coQuyTrinh = false;
-				}
-								
-				if (StringUtils.isNotBlank(trangThaiDon)) {
-					if ("DANG_XU_LY".equals(trangThaiDon) || "DANG_GIAI_QUYET".equals(trangThaiDon)) {
-						canBoXuLyChiDinh = QDon.don.canBoXuLyChiDinh.id.when(canBoXuLyXLD)					
-								.then(Expressions.numberTemplate(Integer.class, "0"))					
-								.otherwise(Expressions.numberTemplate(Integer.class, "1"));
-						sortOrderDonByCanBo = canBoXuLyChiDinh.asc();
-						sortOrderDon = QDon.don.ngayTiepNhan.desc();
-						listDon = (List<Don>) repo.findAll(donService.predicateFindAll(maDon, tuKhoa, nguonDon, phanLoaiDon, tiepNhanTuNgay, tiepNhanDenNgay,
-								hanGiaiQuyetTuNgay, hanGiaiQuyetDenNgay, tinhTrangXuLy, thanhLapDon, trangThaiDon,
-								phongBanGiaiQuyet, canBoXuLyXLD, phongBanXuLyXLD, coQuanTiepNhanXLD, donViXuLyXLD, 
-								vaiTroNguoiDungHienTai, congChuc.getNguoiDung().getVaiTros(), hoTen, trangThaiDonToanHT, ketQuaToanHT, xuLyRepo, repo, giaiQuyetDonRepo,
-								coQuyTrinh), 
-								sortOrderDonByCanBo, sortOrderDon);
-					} else if ("DA_XU_LY".equals(trangThaiDon) || "DA_GIAI_QUYET".equals(trangThaiDon)) {
-						sortOrderDon = QDon.don.ngayTiepNhan.desc();
-						listDon = (List<Don>) repo.findAll(donService.predicateFindAll(maDon, tuKhoa, nguonDon, phanLoaiDon, tiepNhanTuNgay, tiepNhanDenNgay,
-								hanGiaiQuyetTuNgay, hanGiaiQuyetDenNgay, tinhTrangXuLy, thanhLapDon, trangThaiDon,
-								phongBanGiaiQuyet, canBoXuLyXLD, phongBanXuLyXLD, coQuanTiepNhanXLD, donViXuLyXLD, 
-								vaiTroNguoiDungHienTai, congChuc.getNguoiDung().getVaiTros(), hoTen, trangThaiDonToanHT, ketQuaToanHT, xuLyRepo, repo, giaiQuyetDonRepo,
-								coQuyTrinh), 
-								sortOrderDon);
-					}
-				}
+				sortOrderDon = QDon.don.ngayTiepNhan.desc();
+				listDon = (List<Don>) repo.findAll(donService.predicateFindAllDonTraCuu(maDon, tuKhoa, nguonDon, phanLoaiDon, linhVucId, 
+						linhVucChiTietId, tuNgay, denNgay, tinhTrangXuLy, donViXuLyXLD, hoTen, ketQuaToanHT, 
+						taiDonVi, listDonViTiepNhan, xuLyRepo, repo, giaiQuyetDonRepo), 
+						sortOrderDon);
 				
 				int start = pageable.getOffset();
 				int end = (start + pageable.getPageSize()) > listDon.size() ? listDon.size() : (start + pageable.getPageSize());
+				List<Medial_DonTraCuu> listMedial = new ArrayList<Medial_DonTraCuu>();
+				Medial_DonTraCuu media = null;
+				for (Don don : listDon.subList(start, end)) {
+					media = new Medial_DonTraCuu();
+					media.copyDon(don);
+					listMedial.add(media);
+				}
+				Page<Medial_DonTraCuu> pages = new PageImpl<Medial_DonTraCuu>(listMedial, pageable, listDon.size());
 				
-				Page<Don> pages = new PageImpl<Don>(listDon.subList(start, end), pageable, listDon.size());
-				
-				return assembler.toResource(pages, (ResourceAssembler) eass);
+				return mediaTraCuuDonAssembler.toResource(pages, (ResourceAssembler) eass);
 			}
 			return Utils.responseErrors(HttpStatus.FORBIDDEN, ApiErrorEnum.ROLE_FORBIDDEN.name(),
 					ApiErrorEnum.ROLE_FORBIDDEN.getText(), ApiErrorEnum.ROLE_FORBIDDEN.getText());
